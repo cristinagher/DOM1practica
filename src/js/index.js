c171a93398f6b4a740e86ddf294dd07e58f42142
cristinagher <cristinagamezhernandez@gmail.com>
2024-06-24 16:47:56 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,11 @@ console.log(
   `Soy un ${titleElement.tagName} con el id ${titleElement.id}, y la clase ${titleElement.className}. Mi padre es un ${titleElement.parentElement.tagName} con el id ${titleElement.parentElement.id} y la clase ${titleElement.parentElement.className}. Mi hermano es un ${titleElement.previousElementSibling.previousElementSibling.tagName} con el id ${titleElement.previousElementSibling.previousElementSibling.id} y clase ${titleElement.previousElementSibling.previousElementSibling.className}. Mi hermano ${titleElement.previousElementSibling.tagName} con id ${titleElement.previousElementSibling.id} y clase ${titleElement.previousElementSibling.className}.`
 );
 
-const ulElement = document.getElementById("first-list");
-const ulElement2 = document.getElementById("second-list");
+const firstListElement = document.getElementById("first-list");
+const secondListElement = document.getElementById("second-list");
 
-ulElement.textContent = "Second List - Item 1";
-console.log(ulElement);
+const firstListElementContent=firstElement.textContent;
+const secondListElementContent=seconElement.textContent;
+
+firstListElement.textContent=secondListElementContent;
+secondListElement.textContent=firstListElementContent;
